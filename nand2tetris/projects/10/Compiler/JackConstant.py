b26030c0684aaa5b04130aabd0cb60976824721e
WEN1WE <1935862865@qq.com>
2018-11-10 16:38:50 +0800

    Project 10

---
+++ nand2tetris/projects/10/Compiler/JackConstant.py
@@ -0,0 +1,14 @@
+
+# Token types
+KEYWORD = 0
+SYMBOL = 1
+IDENTIFIER = 2
+INT_CONST = 3
+STRING_CONST = 4
+
+keywords = {"class", "constructor", "function", "method", "field", "static", "var", "int", "char", "boolean",
+            "void", "true", "false", "null", "this", "let", "do", "if", "else", "while", "return"}
+
+symbols = {'{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '<', '>', '=', '~'}
+
+tokens = ['keyword', 'symbol', 'identifier', 'integerConstant', 'stringConstant']
